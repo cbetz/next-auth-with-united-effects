e1b39a1f5e8106d13ef7a4e4293085576f54b1e4
Christopher Betz <scrapdog@gmail.com>
2022-05-10 12:50:34 -0400

    scope tweak

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -50,7 +50,7 @@ export default NextAuth({
       name: "United Effects",
       wellKnown: `${process.env.UNITED_EFFECTS_ISSUER}/.well-known/openid-configuration`,
       type: "oauth",
-      authorization: { params: { scope: "openid email username" } },
+      authorization: { params: { scope: "openid email username access" } },
       checks: ["pkce", "state"],
       idToken: true,
       profile(profile) {
